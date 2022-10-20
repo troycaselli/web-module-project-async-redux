428acdcf0e51f17375d99bb9be236324746f074d
Troy Caselli <troycaselli@Troys-MacBook-Air.local>
2022-10-20 14:33:17 -0700

    added styling for the header

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
 import React from 'react';
 import RandomZooAnimal from './components/RandomZooAnimal';
-import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <h1>Bring the Zoo to You!</h1>
       <RandomZooAnimal />
     </div>
   );
--- src/components/RandomZooAnimal.js
+++ src/components/RandomZooAnimal.js
@@ -2,39 +2,54 @@ import React from 'react';
 import {connect} from 'react-redux';
 import {getAnimal} from '../actions/index.actions';
 
+import './RandomZooAnimal.css';
+
 const RandomZooAnimal = (props) => {
     const {animal, isFetching, error, getAnimal} = props
 
-    if(isFetching) return <h3>Your Animal is Being Shipped...</h3>
+    if(isFetching) {
+        return (
+            <header id='fetching'>
+                <h1>Bring the Zoo to You!</h1>
+                <h2>Your Animal is Being Shipped...</h2>
+            </header>
+        )
+    }
 
     if(error) {
         return (
-            <div>
-                <h3>Oh, No! Your Animal Escaped!</h3>
-                <h5>Error: {error}</h5>
-            </div>
+            <header id='error'>
+                <h1>Bring the Zoo to You!</h1>
+                <h2>Oh, No! Your Animal Escaped!</h2>
+                <h3>Error: {error}</h3>
+            </header>
         )
     }
 
     return(
         <div>
-            <button onClick={getAnimal}>Get New Animal</button>
-            <div>
-                <h2>{animal.name}</h2>
-                <h4>(<i>{animal.latin_name}</i>)</h4>
-            </div>
-            <img src={animal.image_link} alt={animal.name} />
-            <div>
-                <h6>Details:</h6>
-                <p>Class: {animal.animal_type}</p>
-                <p>Active: {animal.active_time}</p>
-                <p>Length: {animal.length_min} ft. - {animal.length_max} ft.</p>
-                <p>Weight: {animal.weight_min} lbs. - {animal.weight_max} lbs.</p>
-                <p>Lifespan: {animal.lifespan} years</p>
-                <p>Diet: {animal.diet}</p>
-                <p>Location: {animal.geo_range}</p>
-                <p>Habitat: {animal.habitat}</p>
-            </div>
+            <header>
+                <h1>Bring the Zoo to You!</h1>
+                <button onClick={getAnimal}>Get New Animal</button>
+            </header>
+            <section>
+                <div>
+                    <h2>{animal.name}</h2>
+                    <h3>(<i>{animal.latin_name}</i>)</h3>
+                </div>
+                <img src={animal.image_link} alt={animal.name} />
+                <div>
+                    <h3>Details:</h3>
+                    <p>Class: {animal.animal_type}</p>
+                    <p>Active: {animal.active_time}</p>
+                    <p>Length: {animal.length_min} ft. - {animal.length_max} ft.</p>
+                    <p>Weight: {animal.weight_min} lbs. - {animal.weight_max} lbs.</p>
+                    <p>Lifespan: {animal.lifespan} years</p>
+                    <p>Diet: {animal.diet}</p>
+                    <p>Location: {animal.geo_range}</p>
+                    <p>Habitat: {animal.habitat}</p>
+                </div>
+            </section>
         </div>
     )
 }
