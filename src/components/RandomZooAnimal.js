417b045aaf6f35def3c8ac502184ed763a01b596
Troy Caselli <troycaselli@Troys-MacBook-Air.local>
2022-10-20 17:20:32 -0700

    css styling added to page

--- src/components/RandomZooAnimal.js
+++ src/components/RandomZooAnimal.js
@@ -27,27 +27,32 @@ const RandomZooAnimal = (props) => {
     }
 
     return(
-        <div>
+        <div className='flex-wrapper'>
             <header>
                 <h1>Bring the Zoo to You!</h1>
                 <button onClick={getAnimal}>Get New Animal</button>
             </header>
-            <section>
-                <div>
+            <section id='card-container'>
+                <div className='flex-wrapper'>
                     <h2>{animal.name}</h2>
                     <h3>(<i>{animal.latin_name}</i>)</h3>
                 </div>
                 <img src={animal.image_link} alt={animal.name} />
-                <div>
-                    <h3>Details:</h3>
-                    <p>Class: {animal.animal_type}</p>
-                    <p>Active: {animal.active_time}</p>
-                    <p>Length: {animal.length_min} ft. - {animal.length_max} ft.</p>
-                    <p>Weight: {animal.weight_min} lbs. - {animal.weight_max} lbs.</p>
-                    <p>Lifespan: {animal.lifespan} years</p>
-                    <p>Diet: {animal.diet}</p>
-                    <p>Location: {animal.geo_range}</p>
-                    <p>Habitat: {animal.habitat}</p>
+                <h3>Details</h3>
+                <hr />  
+                <div className='wrapper'>
+                    <div className='half'>
+                        <p><b>Class: </b>{animal.animal_type}</p>
+                        <p><b>Active: </b>{animal.active_time}</p>
+                        <p><b>Length: </b>{animal.length_min} ft. - {animal.length_max} ft.</p>
+                        <p><b>Weight: </b>{animal.weight_min} lbs. - {animal.weight_max} lbs.</p>
+                    </div>
+                    <div className='half'>
+                        <p><b>Lifespan: </b>{animal.lifespan} years</p>
+                        <p><b>Diet: </b>{animal.diet}</p>
+                        <p><b>Location: </b>{animal.geo_range}</p>
+                        <p><b>Habitat: </b>{animal.habitat}</p>
+                    </div>
                 </div>
             </section>
         </div>
